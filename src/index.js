769117582c4f8698ef7bcb787585e15931afaca9
AlexOlga <bulbenko@yandex.ru>
2022-01-04 09:46:51 +0300

    feat:revers number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  const str = `${n}`
+  let newStr ='';
+  let min;
+  (n >= 0) ? min=0: min=1;
+    for (let i = str.length - 1; i >= min; i-- ) {
+     newStr = newStr + str[i];
+  }
+ 
+  return (n >= 0) ? Number(newStr) : Number(newStr);
 }
